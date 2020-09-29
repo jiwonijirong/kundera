99abacf39e73f9a4c4c4af110db367ff0b39b52f
Jiwon Kim <jiwonkim@Jiwons-MacBook-Pro.local>
2020-09-29 15:15:10 +0900

    initial commit

---
+++ src/main/java/com/saturn/kundera/KunderaApplication.java
@@ -0,0 +1,13 @@
+package com.saturn.kundera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunderaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KunderaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saturn/kundera/KunderaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saturn.kundera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunderaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
